e476a25cd2dce2c4535e65962f6f5c5d6b376015
Mateus Santos Lemos <mateus.santos.lemos@avanade.com>
2025-01-30 21:22:38 -0300

    refactor: removido o print do código de extração de informação

--- information_extractor.py
+++ information_extractor.py
@@ -13,7 +13,6 @@ class InformationExtractor:
         phone_number_list = phone_number_extractor.extract(text)
         matches = []
         for groups in phone_number_list:
-            print(groups)
             phone_number = '-'.join([groups[1], groups[3], groups[5]])
             if groups[8] != '':
                 phone_number += ' x' + groups[8]
@@ -22,7 +21,6 @@ class InformationExtractor:
         email_extractor = EmailExtractor()
         email_list = email_extractor.extract(text)
         for groups in email_list:
-            print(groups)
             matches.append(groups[0])
         
         return matches
